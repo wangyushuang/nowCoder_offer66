e4a23df5c694ebb8b70d258d6db09f757846b712
wangyushuang <wangyushuang_guyan@126.com>
2017-10-15 20:25:11 +0800

    重建二叉树
    输入某二叉树的前序遍历和中序遍历的结果，请重建出该二叉树。假设输入的前序遍历和中序遍历的结果中都不含重复的数字。例如输入前序遍历序列{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}，则重建二叉树并返回。

---
+++ reConBT.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+import java.util.ArrayList;
+public class reConBT {
+    public TreeNode reConstructBinaryTree(int [] pre,int [] in) 
+    {
+        if(pre.length==0)
+            return null;
+        TreeNode root=new TreeNode(pre[0]);
+        boolean flag=false;
+        ArrayList<Integer> lInList=new ArrayList();//左子树的中序遍历
+        ArrayList<Integer> rInList=new ArrayList();//右子树的中序遍历
+        for(int i=0;i<in.length;i++)
+        {
+            if(in[i]==pre[0])
+            {
+                flag=true;
+                continue;
+            }
+            if(!flag)
+                lInList.add(in[i]);
+            else
+                rInList.add(in[i]);
+        }
+        int[] lIn=new int[lInList.size()];//左子树的中序遍历
+        int[] rIn=new int[rInList.size()];//右子树的中序遍历
+        int[] lPre=new int[lInList.size()];//左子树的前序遍历
+        int[] rPre=new int[rInList.size()];//右子树的前序遍历
+        for(int i=0;i<lInList.size();i++)
+        {
+            lIn[i]=lInList.get(i).intValue();
+            lPre[i]=pre[i+1];
+        }
+        for(int i=0;i<rInList.size();i++)
+        {
+            rIn[i]=rInList.get(i).intValue();
+            rPre[i]=pre[i+1+lInList.size()];
+        }
+        root.left=reConstructBinaryTree(lPre,lIn);
+        root.right=reConstructBinaryTree(rPre,rIn);
+        return root;
+        
+    }
+}
